b74c4947bb37fb9e6056ce10fc1e25a1fbb937f3
JuanJRGutierrez <39140427+JuanJRGutierrez@users.noreply.github.com>
2018-05-30 18:54:32 -0400

    Add files via upload

---
+++ My Online Library/inc/data.php
@@ -0,0 +1,179 @@
+<?php
+//Books*
+$catalog[101] = [
+    "title" => "Ready Player One",
+    "img" => "../img/media/readyplayerone.png",
+   "genre" => "LitRPG, Science fiction, dystopian",
+   "format" => "Paperback",
+   "year" => 2011,
+   "category" => "Books",
+   "authors" => [
+       "Ernest Cline"
+   ],
+   "publisher" => "Random House",
+   "isbn" => '9780307887436'
+];
+
+$catalog[102] = [
+    "title" => "Reality is Broken: Why Video Games Make Us Better and How They Can Change the World",
+    "img" => "../img/media/realityisbroken.png",
+   "genre" => "Psychology",
+   "format" => "Paperback",
+   "year" => 2011,
+   "category" => "Books",
+   "authors" => [
+       "Jane McGonigal"
+   ],
+   "publisher" => "Penguin",
+   "isbn" => '9781611064292'
+];
+
+$catalog[103] = [
+    "title" => "To Kill a Mockingbird",
+    "img" => "../img/media/tokillamockingbird.png",
+   "genre" => "Southern Gothic, Bildungsroman",
+   "format" => "Paperback",
+   "year" => 1960,
+   "category" => "Books",
+   "authors" => [
+       "Harper Lee"
+   ],
+   "publisher" => "J. B. Lippincott & Co.",
+   "isbn" => '9781611064292'
+];
+
+$catalog[104] = [
+    "title" => "The Best American Travel Writing",
+    "img" => "../img/media/travelwriting.png",
+   "genre" => "Travel Literature",
+   "format" => "Paperback",
+   "year" => 2017,
+   "category" => "Books",
+   "authors" => [
+       "Lauren Collins"
+   ],
+   "publisher" => "Houghton Mifflin Harcourt",
+   "isbn" => '9781611064653'
+];
+
+//Movies*
+$catalog[201] = [
+   "title" => "Body Heat",
+   "img" => "../img/media/bodyheat.png",
+   "genre" => "Drama,Thriller",
+   "format" => "DVD",
+   "year" => 1981,
+   "category" => "Movies",
+   "director" => "Lawrence Kasdan",
+   "writers" => [
+       "Lawrence Kasdan"
+
+   ],
+   "stars" => [
+       "William Hurt",
+       "Kathleen Turner",
+       "Richard Crenna"
+   ]
+];
+
+$catalog[202] = [
+   "title" => "Deadpool",
+   "img" => "../img/media/deadpool.png",
+   "genre" => "Fantasy,Science-Fiction",
+   "format" => "DVD",
+   "year" => 2016,
+   "category" => "Movies",
+   "director" => "Tim Miller",
+   "writers" => [
+       "Rhett Reese",
+		"Paul Wernick"
+
+   ],
+   "stars" => [
+       "Ryan Reynolds",
+       "Morena Baccarin",
+       "Ed Skrein",
+		"T.J. Miller",
+		"Gina Carano",
+		"Brianna Hildebrand",
+		"Stefan Kapičić",
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Ferris Bueller's Day Off",
+   "img" => "../img/media/ferrisbuellersdayoff.png",
+   "genre" => "Drama, Teen Film",
+   "format" => "DVD",
+   "year" => 1986,
+   "category" => "Movies",
+   "director" => "John Hughes",
+   "writers" => [
+       "John Hughes",
+
+   ],
+   "stars" => [
+       "Matthew Broderick",
+       "Alan Ruck",
+       "Mia Sara"
+   ]
+];
+
+$catalog[204] = [
+   "title" => "John Wick",
+   "img" => "../img/media/johnwick.png",
+   "genre" => "Crime Film, Thriller",
+   "format" => "DVD",
+   "year" => 2014,
+   "category" => "Movies",
+   "director" => "Chad Stahelski, David Leitch",
+   "writers" => [
+       "Derek Kolstad",
+
+   ],
+   "stars" => [
+       "Keanu Reeves",
+   ]
+];
+
+//Music*
+$catalog[301] = [
+   "title" => "A Kind of Magic",
+   "img" => "img/media/queen_akindofmagic.png",
+   "genre" => "Rock",
+   "format" => "CD",
+   "year" => 1986,
+   "category" => "Music",
+   "artist" => "Queen"
+];
+
+$catalog[302] = [
+   "title" => "Made in Heaven",
+   "img" => "../img/media/queen_madeinheaven.png",
+   "genre" => "Rock",
+   "format" => "CD",
+   "year" => 1995,
+   "category" => "Music",
+   "artist" => "Queen"
+];
+
+$catalog[303] = [
+   "title" => "News of the World",
+   "img" => "../img/media/queen_newsoftheworld.png",
+   "genre" => "Rock",
+   "format" => "CD",
+   "year" => 1977,
+   "category" => "Music",
+   "artist" => "Queen"
+];
+
+$catalog[304] = [
+   "title" => "Queen",
+   "img" => "../img/media/queen_queen.png",
+   "genre" => "Rock",
+   "format" => "CD",
+   "year" => 1973,
+   "category" => "Music",
+   "artist" => "Queen"
+];
+?>
